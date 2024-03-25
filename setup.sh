481fcaa4ea473f5e9077c8a31e9ba7bd0acf38fb
Ali Emir Güzey <git@guezey.me>
2024-03-25 20:02:34 +0300

    update setup script

--- setup.sh
+++ setup.sh
@@ -2,16 +2,18 @@
 
 # Create a .bash_aliases file
 cp bash_aliases ~/.bash_aliases
-echo ".bash_aliases file created"
+echo ".bash_aliases file created/updated"
 
 if [ ! -f ~/.bashrc ]; then
     touch ~/.bashrc
     echo ".bashrc file created"
 fi
 
-bashrc_file="$HOME/.bashrc"
-alias_snippet='if [ -f ~/.bash_aliases ]; then\n    . ~/.bash_aliases\nfi'
-
-echo "$alias_snippet" >> $bashrc_file 
-echo "Aliases added to .bashrc file"
+# Check if the alias snippet is already in the .bashrc file
+cat ~/.bashrc | grep -q "\. ~/\.bash_aliases"
+if [ $? -ne 0 ]; then
+  # Add the alias snippet to the .bashrc file
+  echo ". ~/.bash_aliases" >> $bashrc_file
+  echo "Alias snippet added to .bashrc file"
+fi
 
